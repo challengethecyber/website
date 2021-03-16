62d4d780ec08135ead973b116b4e825f914c1f86
Olaf Haalstra <olafhaalstra@gmail.com>
2021-03-17 00:44:04 +0100

    Added contentful example

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,57 @@
+import React, { FC } from "react"
+import { GatsbyImage } from "gatsby-plugin-image"
+import { graphql, Link } from "gatsby"
+import PropTypes from "prop-types"
+
+interface ICardProps {
+  name: string
+  slug: string
+  summary: string
+  image: {
+    layout: any
+    width: number
+    height: number
+    images: any
+  }
+}
+
+const Card: FC<ICardProps> = ({ name, slug, summary, image }) => {
+  return (
+    <div className="bg-white h-full shadow-sm rounded-md overflow-hidden group">
+      <Link to={`/${slug}`}>
+        <div className="group-hover:opacity-75 transition duration-150 ease-in-out">
+          {/* <StaticImage fluid={thumbnail.localFile.childImageSharp.fluid} alt={name} /> */}
+          <GatsbyImage image={image} alt={name} />
+        </div>
+        <div className="p-4 sm:p-5">
+          <h1 className="sm:text-lg text-gray-900 font-semibold">{name}</h1>
+          <p className="text-sm sm:text-base text-gray-700">{summary}</p>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default Card
+
+// TODO: Fix image fetch https://www.gatsbyjs.com/docs/reference/release-notes/image-migration-guide/#fluid
+export const query = graphql`
+  fragment PortfolioCard on ContentfulPortfolio {
+    id
+    name
+    slug
+    thumbnail {
+      localFile {
+        childImageSharp {
+          gatsbyImageData(
+            layout: FIXED
+            width: 444
+            placeholder: BLURRED
+            formats: [AUTO, WEBP, AVIF]
+          )
+        }
+      }
+    }
+    summary
+  }
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Card from "../components/Card"
 
-const IndexPage = () => (
+const IndexPage = ({ data }: any) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -19,6 +20,17 @@ const IndexPage = () => (
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
+    <div className="bg-gray-100 py-12 lg:py-16">
+      {data.portfolio && data.portfolio.nodes.length > 0 ? (
+        <div className="flex flex-wrap -mx-3 lg:-mx-6">
+          {data.portfolio.nodes.map((item: any) => (
+            <Card {...item} />
+          ))}
+        </div>
+      ) : (
+        <div className="container">No projects found.</div>
+      )}
+    </div>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
@@ -27,3 +39,13 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query HomeQuery {
+    portfolio: allContentfulPortfolio {
+      nodes {
+        ...PortfolioCard
+      }
+    }
+  }
+`
