b246e9660f69c0f62e878d8e2de70df746a01a8d
Diederik Bakker <diederik@outlook.com>
2021-04-06 00:10:04 +0200

    Remove unnecessary dot, add YouTube to footer

--- src/sections/footer/index.tsx
+++ src/sections/footer/index.tsx
@@ -4,17 +4,6 @@ const Footer = () => {
   return (
     <footer className="bg-white">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-        {/* <nav
-          className="-mx-5 -my-2 flex flex-wrap justify-center"
-          aria-label="Footer"
-        >
-
-          <div className="px-5 py-2">
-            <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-              Blog
-            </a>
-          </div>
-        </nav> */}
         <div className="mt-8 flex justify-center space-x-6">
           <a
             href="https://www.instagram.com/challengethecyber/"
@@ -51,6 +40,21 @@ const Footer = () => {
           </a>
 
           <a
+            href="https://www.youtube.com/channel/UCi0tQlRzNAS5vuhxvkRVQlg"
+            className="text-gray-400 hover:text-gray-500"
+          >
+            <span className="sr-only">YouTube</span>
+            <svg
+              className="h-6 w-6"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 30 30"
+            >
+              <path d="M 15 4 C 10.814 4 5.3808594 5.0488281 5.3808594 5.0488281 L 5.3671875 5.0644531 C 3.4606632 5.3693645 2 7.0076245 2 9 L 2 15 L 2 15.001953 L 2 21 L 2 21.001953 A 4 4 0 0 0 5.3769531 24.945312 L 5.3808594 24.951172 C 5.3808594 24.951172 10.814 26.001953 15 26.001953 C 19.186 26.001953 24.619141 24.951172 24.619141 24.951172 L 24.621094 24.949219 A 4 4 0 0 0 28 21.001953 L 28 21 L 28 15.001953 L 28 15 L 28 9 A 4 4 0 0 0 24.623047 5.0546875 L 24.619141 5.0488281 C 24.619141 5.0488281 19.186 4 15 4 z M 12 10.398438 L 20 15 L 12 19.601562 L 12 10.398438 z" />
+            </svg>
+          </a>
+
+          <a
             href="https://github.com/challengethecyber"
             className="text-gray-400 hover:text-gray-500"
           >
@@ -70,7 +74,7 @@ const Footer = () => {
           </a>
         </div>
         <p className="mt-8 text-center text-base text-gray-400">
-          &copy; {new Date().getFullYear()} Challenge the Cyber.
+          &copy; {new Date().getFullYear()} Challenge the Cyber
         </p>
       </div>
     </footer>
