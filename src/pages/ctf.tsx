b36d23110cf0f1b871349932e5277e7594aac19f
Diederik Bakker <diederik@outlook.com>
2021-04-13 23:31:43 +0200

    Add CTA to CTF page

--- src/pages/ctf.tsx
+++ src/pages/ctf.tsx
@@ -1,6 +1,8 @@
-import React from "react"
+import React, { useContext } from "react"
 
 import { graphql, Node, NodeInput } from "gatsby"
+
+import { EnrollmentContext } from "/src/context/enrollment"
 
 import Page from "components/page"
 import Content from "components/content"
@@ -14,9 +16,30 @@ interface IQueryProps {
 }
 
 const CtfPage = ({ data }: IQueryProps) => {
+  const { setShowEnrollmentSlideOver } = useContext(EnrollmentContext)
+
   return (
     <Page title="CTF">
       <Content data={data.allContentfulContent.nodes[0]} />
+      <div className="">
+        <div className="max-w-3xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <span className="block">Klaar om te spelen?</span>
+            <span className="block text-orange-500">Meld je nu aan.</span>
+          </h2>
+          <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+            <div className="inline-flex rounded-md shadow">
+              <button
+                type="button"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-orange-500 hover:bg-orange-600"
+                onClick={() => setShowEnrollmentSlideOver(true)}
+              >
+                Aanmelden
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </Page>
   )
 }
