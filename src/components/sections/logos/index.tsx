c35a17ca09f56cd804658f6e59947785f287b25d
Diederik Bakker <diederik@outlook.com>
2022-02-04 15:28:46 +0100

    Update index.tsx

--- src/components/sections/logos/index.tsx
+++ src/components/sections/logos/index.tsx
@@ -55,7 +55,7 @@ const Logos = ({ data }: ILogosProps) => {
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <div className="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
           <div>
-            <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+            <h2 id="vrienden" className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
               {data.title}
             </h2>
             <div className="mt-3 max-w-3xl text-lg text-gray-500">
