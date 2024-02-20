1c7dcb465b49ffa8ef6c5e274ec15d5569f32d42
DiedB <diederik@outlook.com>
2024-02-20 18:02:17 +0100

    Fix enrollment confirmation

--- src/components/enrollment-form/success-screen/index.tsx
+++ src/components/enrollment-form/success-screen/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, createRef } from "react"
 
-// import lottie from "lottie-web"
+import lottie from "lottie-web"
 import checkmark from "./animations/checkmark.json"
 
 type ISuccessScreenProps = {}
@@ -8,35 +8,34 @@ type ISuccessScreenProps = {}
 const SuccessScreen = ({}: ISuccessScreenProps) => {
   let animationContainer = createRef<any>()
 
-  // useEffect(() => {
-  //   if (typeof document !== "undefined" && animationContainer) {
-  //     const anim = lottie.loadAnimation({
-  //       container: animationContainer!.current,
-  //       renderer: "svg",
-  //       loop: false,
-  //       autoplay: true,
-  //       animationData: checkmark,
-  //     })
-  //     return () => anim.destroy() // optional clean up for unmounting
-  //   }
-  // }, [animationContainer])
+  useEffect(() => {
+    if (typeof document !== "undefined" && animationContainer) {
+      const anim = lottie.loadAnimation({
+        container: animationContainer!.current,
+        renderer: "svg",
+        loop: false,
+        autoplay: true,
+        animationData: checkmark,
+      })
+      return () => anim.destroy() // optional clean up for unmounting
+    }
+  }, [animationContainer])
 
   return (
-    // <div className="flex flex-col space-y-6 justify-center h-full">
-    //   <div
-    //     className="h-48 fill-current text-orange-500"
-    //     ref={animationContainer}
-    //   />
-    //   <div className="flex flex-col">
-    //     <span className="font-medium text-lg text-center">
-    //       Leuk dat je meedoet!
-    //     </span>
-    //     <span className="font-medium text-gray-700 text-sm text-center">
-    //       Je ontvangt een e-mail ter bevestiging van je aanmelding.
-    //     </span>
-    //   </div>
-    // </div>
-    <div />
+    <div className="flex flex-col space-y-6 justify-center h-full">
+      <div
+        className="h-48 fill-current text-orange-500"
+        ref={animationContainer}
+      />
+      <div className="flex flex-col">
+        <span className="font-medium text-lg text-center">
+          Leuk dat je meedoet!
+        </span>
+        <span className="font-medium text-gray-700 text-sm text-center">
+          Je ontvangt een e-mail ter bevestiging van je aanmelding.
+        </span>
+      </div>
+    </div>
   )
 }
 
